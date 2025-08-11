356e710e8b88a6d3887469d8bab6f03fd70ad00a
VK_POS_2023 <nkaujkiabvang20212022@gmail.com>
2025-08-11 20:27:08 +0700

    comint

--- Admin/Report_Receip.php
+++ Admin/Report_Receip.php
@@ -13,8 +13,8 @@ include('header.php')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="css_js/js/chart_line.js"></script>
     <link href="css_js/css/styles.css" rel="stylesheet" />
-	<link rel="stylesheet" href="assets/adminlte.min.css">
-	<link rel="stylesheet" href="assets/adminlte.min.css">
+	<!-- <link rel="stylesheet" href="assets/adminlte.min.css"> -->
+	<!-- <link rel="stylesheet" href="assets/adminlte.min.css"> -->
   <link rel="stylesheet" href="css_js/css/chart.css"></link>
 
   <script src="css_js/js/highcharts.js"></script>
